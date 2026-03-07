1da31ea2d38df0cdf3abcf0daa08110ae677956b
Si-Xiaoming <zbssm123@163.com>
2026-03-07 21:42:38 +0100

    fix: fix bugs about formula

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,13 +1,9 @@
 window.MathJax = {
     tex: {
-        inlineMath: [["\\(", "\\)"]],
-        displayMath: [["\\[", "\\]"]],
+        inlineMath: [["\\(", "\\)"], ["$", "$"]],
+        displayMath: [["\\[", "\\]"], ["$$", "$$"]],
         processEscapes: true,
         processEnvironments: true
-    },
-    options: {
-        ignoreHtmlClass: ".*|",
-        processHtmlClass: "arithmatex"
     }
 };
 
